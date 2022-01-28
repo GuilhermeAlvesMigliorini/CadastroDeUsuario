fe513bbc5418208e3b741a828ea6ba86a3fbfccf
GuilhermeAlves <alvesmigliorini@hotmail.com>
2022-01-28 16:52:23 -0300

    cadastro implementado

---
+++ src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+<footer className="footer">
+  <spam>
+    Desenvolvido com <i className='fa fa-heart text-danger'></i> por
+    <strong> Cod<spam className='text-danger'>3</spam>r</strong>
+  </spam>
+</footer>
